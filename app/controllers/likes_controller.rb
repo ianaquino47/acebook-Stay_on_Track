fb25d8b19d0b11fe4313317bb210d191dafa95fe
iAquino47 <ianaquino47@gmail.com>
2019-08-23 11:14:39 +0100

    added likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,24 @@
 class LikesController < ApplicationController
+  before_action :find_post
   def create
-    p "HEREEEERREERERERE"
-    p params[:user_id]
-    @post = params[:post]
-    @comment = @post.likes.create!(user_id: session[:user_id])
+    if already_liked?
+      @like = @post.likes.find_by(user_id: current_user.id)
+      @like.destroy
+    else
+      @post.likes.create(user_id: current_user.id)
+    end
     redirect_to '/'
   end
 
-  def destroy
+
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
   end
 end
