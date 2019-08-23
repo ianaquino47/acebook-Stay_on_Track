d5279e110cbd8025dc38241348dc756714ae7b63
Freddie <f.varnals@gmail.com>
2019-08-23 11:56:16 +0100

    User can add a profile picture to their account

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2019_08_22_132212) do
-
-
+ActiveRecord::Schema.define(version: 2019_08_22_202757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
